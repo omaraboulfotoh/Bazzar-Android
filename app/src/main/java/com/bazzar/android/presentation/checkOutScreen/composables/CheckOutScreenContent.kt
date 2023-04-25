9a8f2e0594c90190a8dbf6b5b471ab1d3b6d7dbd
Shafie <ashafie@banquemisr.com>
2023-04-25 23:16:54 +0200

    CheckOut Screen update

--- app/src/main/java/com/bazzar/android/presentation/checkOutScreen/composables/CheckOutScreenContent.kt
+++ app/src/main/java/com/bazzar/android/presentation/checkOutScreen/composables/CheckOutScreenContent.kt
@@ -1,20 +1,13 @@
 package com.bazzar.android.presentation.checkOutScreen.composables
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import com.bazzar.android.R
-import com.bazzar.android.presentation.addressScreen.composables.AddressInputViews
 import com.bazzar.android.presentation.checkOutScreen.CheckOutContract
-import com.bazzar.android.presentation.composables.PrimaryButton
 import com.bazzar.android.presentation.theme.BazzarTheme
 
 @Composable
@@ -30,15 +23,5 @@ fun CheckOutScreenContent(
     ) {
         CheckOutHeader()
         AddressView()
-        PrimaryButton(
-            text = stringResource(id = R.string.continue_to_pay),
-            onClick = { /*TODO*/ },
-            textColor = Color.White,
-            background = colorResource(id = R.color.prussian_blue),
-            modifier = Modifier
-                .height(65.dp)
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(32.5.dp))
-        )
     }
 }
