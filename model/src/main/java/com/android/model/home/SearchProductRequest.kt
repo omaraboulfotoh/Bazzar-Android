ab6a68bb44ea4e17e04bd96bb6fe784612d0a04c
omar aboulfotoh <omaraboulfotoh7@gmail.com>
2023-04-26 18:18:56 +0200

    handle the search request

--- model/src/main/java/com/android/model/home/SearchProductRequest.kt
+++ model/src/main/java/com/android/model/home/SearchProductRequest.kt
@@ -9,8 +9,12 @@ import kotlinx.android.parcel.Parcelize
 @JsonClass(generateAdapter = true)
 data class SearchProductRequest(
     @Json(name = "categoryId") val categoryId: Int? = null,
-    @Json(name = "brandList") val brandList: List<Int>? = null,
+    @Json(name = "brandList") val brandList: List<Int> = emptyList(),
     @Json(name = "searchKey") val searchKey: String? = null,
     @Json(name = "pageSize") val pageSize: Int = 20,
-    @Json(name = "pageIndex") val pageIndex: Int = 0
+    @Json(name = "pageIndex") val pageIndex: Int = 0,
+    @Json(name = "maxPrice") val maxPrice: Int = 0,
+    @Json(name = "minPrice") val minPrice: Int = 0,
+    @Json(name = "sizeList") val sizeList: List<Int> = emptyList(),
+    @Json(name = "colorList") val colorList: List<Int> = emptyList(),
 ) : Parcelable
