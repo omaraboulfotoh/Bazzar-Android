71be30c11351ce99f48b95063add2e8b8080d963
omar aboulfotoh <omaraboulfotoh7@gmail.com>
2023-05-28 22:56:52 +0300

    update app version

--- buildSrc/src/main/java/com/android/buildSrc/Configs.kt
+++ buildSrc/src/main/java/com/android/buildSrc/Configs.kt
@@ -6,7 +6,7 @@ object Configs {
     const val applicationId = "com.bazzar.android"
     const val minSdkVersion = 24
     const val targetSdkVersion = 33
-    const val versionCode = 7
-    const val versionName = "1.0.5"
+    const val versionCode = 8
+    const val versionName = "1.0.6"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
