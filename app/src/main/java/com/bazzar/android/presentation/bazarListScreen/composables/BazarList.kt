e9c3f448bf3512260f3590c5e10ad902bce2cd2d
omar aboulfotoh <omaraboulfotoh7@gmail.com>
2023-06-03 20:31:27 +0300

    enhance bazzars list search view

--- app/src/main/java/com/bazzar/android/presentation/bazarListScreen/composables/BazarList.kt
+++ app/src/main/java/com/bazzar/android/presentation/bazarListScreen/composables/BazarList.kt
@@ -2,9 +2,12 @@ package com.bazzar.android.presentation.bazarListScreen.composables
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.android.model.home.BazaarModel
@@ -20,13 +23,16 @@ fun BazarList(
     LazyVerticalGrid(
         modifier = modifier,
         columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(BazzarTheme.spacing.m),
+        contentPadding = PaddingValues(BazzarTheme.spacing.xs),
         verticalArrangement = Arrangement.spacedBy(BazzarTheme.spacing.m),
     ) {
-        items(bazarList) { item ->
+        itemsIndexed(bazarList) { index, item ->
             BazarListItem(item = item,
                 onClick = { onBazarItemClick.invoke(item) },
                 onFavClicked = { onBazarItemFavClick(item) })
+            if (index == bazarList.lastIndex) {
+                Spacer(modifier = Modifier.height(BazzarTheme.spacing.m))
+            }
         }
     }
 }
--- app/src/main/java/com/bazzar/android/presentation/bazarListScreen/composables/BazarListScreenContent.kt
+++ app/src/main/java/com/bazzar/android/presentation/bazarListScreen/composables/BazarListScreenContent.kt
@@ -2,17 +2,25 @@ package com.bazzar.android.presentation.bazarListScreen.composables
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import com.bazzar.android.R
 import com.bazzar.android.presentation.bazarListScreen.BazarListContract
@@ -27,7 +35,11 @@ fun BazarListScreenContent(
     onSendEvent: (BazarListContract.Event) -> Unit
 ) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = BottomNavigationHeight),
+        verticalArrangement = Arrangement.spacedBy(BazzarTheme.spacing.m),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         BazzarAppBar(
             title = stringResource(id = R.string.bazzar),
@@ -35,23 +47,31 @@ fun BazarListScreenContent(
         )
         SearchTextInput(
             modifier = Modifier
-                .padding(top = BazzarTheme.spacing.l)
+                .defaultMinSize(minHeight = 36.dp)
+                .padding(vertical = BazzarTheme.spacing.xs)
                 .padding(horizontal = BazzarTheme.spacing.m)
                 .fillMaxWidth()
+                .clip(RoundedCornerShape(24.dp))
+                .background(BazzarTheme.colors.white)
                 .border(
-                    width = 1.dp,
+                    width = 0.5.dp,
                     color = BazzarTheme.colors.stroke,
                     shape = RoundedCornerShape(24.dp)
                 ),
-            childRowModifier = Modifier.padding(vertical = BazzarTheme.spacing.m),
             hint = stringResource(id = R.string.search_by_bzar_name),
             value = state.searchTerm ?: "",
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+            childRowModifier = Modifier.padding(vertical = BazzarTheme.spacing.xs),
             onValueChange = { onSendEvent(BazarListContract.Event.OnSearchTermChanged(it)) },
-            textStyle = BazzarTheme.typography.body2.copy(color = BazzarTheme.colors.primaryButtonColor),
+            textStyle = BazzarTheme.typography.captionBold.copy(color = BazzarTheme.colors.primaryButtonColor),
             cursorColor = SolidColor(BazzarTheme.colors.primaryButtonColor),
             leadingIcon = {
                 Icon(
-                    modifier = Modifier.padding(horizontal = BazzarTheme.spacing.s),
+                    modifier = Modifier
+                        .padding(
+                            start = BazzarTheme.spacing.briefingIconsDimen,
+                            end = BazzarTheme.spacing.s
+                        ),
                     painter = painterResource(id = R.drawable.search_icon),
                     tint = BazzarTheme.colors.primaryButtonColor,
                     contentDescription = ""
@@ -61,8 +81,7 @@ fun BazarListScreenContent(
         BazarList(
             modifier = Modifier
                 .weight(1f)
-                .background(color = BazzarTheme.colors.backgroundColor)
-                .padding(bottom = BottomNavigationHeight),
+                .background(color = BazzarTheme.colors.backgroundColor),
             bazarList = state.availableBazars.orEmpty(),
             onBazarItemFavClick = {
                 onSendEvent(BazarListContract.Event.OnBazarItemFavClicked(it))
